4776e583e283d31580d191ff346f0e8d0ed025a3
Thomas Hempel <thomas@scriptme.de>
2013-03-30 12:07:47 +0100

    [NEW] Adds some more demo setup

--- Classes/Scriptme/MarkdownContent/Controller/StandardController.php
+++ Classes/Scriptme/MarkdownContent/Controller/StandardController.php
@@ -1,12 +1,9 @@
 <?php
+
 namespace Scriptme\MarkdownContent\Controller;
 
-/*                                                                        *
- * This script belongs to the TYPO3 Flow package "Scriptme.MarkdownContent".*
- *                                                                        *
- *                                                                        */
-
 use TYPO3\Flow\Annotations as Flow;
+use \Scriptme\MarkdownContent\Utility\Files as Files;
 
 /**
  * Standard controller for the Scriptme.MarkdownContent package 
@@ -16,14 +13,37 @@ use TYPO3\Flow\Annotations as Flow;
 class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 
 	/**
+	 * @var \TYPO3\Flow\Session\SessionInterface
+	 * @Flow\Inject
+	 */
+	protected $session;
+
+	/**
 	 * Index action
 	 *
+	 * @Flow\Session(autoStart = TRUE)
+	 *
 	 * @param string $path
+	 *
 	 * @return void
 	 */
-	public function indexAction($path = '') {
+	public function indexAction($path = '')
+	{
+		$packageKey = $this->controllerContext->getRequest()->getControllerPackageKey();
+		$subpackageKey = $this->controllerContext->getRequest()->getControllerSubpackageKey();
+		$baseDirectory = Files::contentBaseDirectory($packageKey, $subpackageKey);
+
+		$fsPath = str_replace('-', '/', urldecode($path));
+
+		$metaData = Files::fetchMetaInformationForDirectory($baseDirectory . '/' . $fsPath);
+		// $contents = Files::getFilesInPath($baseDirectory . '/' . $path, 'md');
+
+		$this->session->putData('currentPath', $path);
 		$this->view->assign('currentPath', urldecode($path));
+		$this->view->assign('metaData', $metaData);
 	}
+
+
 
 }
 
