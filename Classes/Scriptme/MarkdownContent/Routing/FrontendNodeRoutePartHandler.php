e6d3c0a09bcfa4c26e003ed641182b2bfd65e3b5
Thomas Hempel <thomas@scriptme.de>
2013-04-02 19:47:00 +0200

    [NEW] Adds a custom route part handler to be able to handle sane paths in the urls
    Attention this might break other packages!

---
+++ Classes/Scriptme/MarkdownContent/Routing/FrontendNodeRoutePartHandler.php
@@ -0,0 +1,36 @@
+<?php
+namespace Scriptme\MarkdownContent\Routing;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "TYPO3.Neos".            *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * A route part handler for finding nodes specifically in the website's frontend.
+ *
+ * @Flow\Scope("singleton")
+ */
+class FrontendNodeRoutePartHandler extends \TYPO3\Flow\Mvc\Routing\DynamicRoutePart
+{
+
+	/**
+	 * Extracts the node path from the request path.
+	 *
+	 * @param string $requestPath The request path to be matched
+	 * @return string value to match, or an empty string if $requestPath is empty or split string was not found
+	 */
+	protected function findValueToMatch($requestPath)
+	{
+		return $requestPath;
+	}
+
+}
+?>
