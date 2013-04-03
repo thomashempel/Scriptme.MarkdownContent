cd4de199e84a9695269e1e4046459efa3adae0b3
Thomas Hempel <thomas@scriptme.de>
2013-04-03 20:30:55 +0200

    [NEW] Adds support for external packages
    With this change set, external packages can provide all the content and structure for a website.

--- Classes/Scriptme/MarkdownContent/Controller/StandardController.php
+++ Classes/Scriptme/MarkdownContent/Controller/StandardController.php
@@ -19,6 +19,19 @@ class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 	protected $session;
 
 	/**
+	 * @var array
+	 */
+	protected $settings;
+
+	/**
+	 * @param array $settings
+	 */
+	public function injectSettings(array $settings)
+	{
+		$this->settings = $settings;
+	}
+
+	/**
 	 * Index action
 	 *
 	 * @Flow\Session(autoStart = TRUE)
@@ -29,22 +42,58 @@ class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 	 */
 	public function indexAction($path = '')
 	{
-		$packageKey = $this->controllerContext->getRequest()->getControllerPackageKey();
-		$subpackageKey = $this->controllerContext->getRequest()->getControllerSubpackageKey();
-		$baseDirectory = Files::contentBaseDirectory($packageKey, $subpackageKey);
+		$externalPackage = FALSE;
+
+		if ($this->settings['SitePackage']) {
+			$packageKey = $this->settings['SitePackage'];
+			$subpackageKey = NULL;
+			$externalPackage = TRUE;
+		} else {
+			$packageKey = $this->controllerContext->getRequest()->getControllerPackageKey();
+			$subpackageKey = $this->controllerContext->getRequest()->getControllerSubpackageKey();
+		}
+
+		$packageResourcesDirectory = Files::packageResourcesDirectory($packageKey);
+		$packageContentDirectory = Files::packageContentDirectory($packageKey, $subpackageKey);
 
 		$path = str_replace(array('../', '.html'), '', $path);
-		$fsPath = $baseDirectory . '/' . $path;
+		$pagePathInFilesystem = $packageContentDirectory . $path;
 
-		if (!file_exists($fsPath) || !is_dir($fsPath)) {
+		// var_dump(array($packageResourcesDirectory, $packageContentDirectory), $pagePathInFilesystem);
+
+		if (!file_exists($pagePathInFilesystem) || !is_dir($pagePathInFilesystem)) {
 			$this->throwStatus(404);
 		}
 
-		$metaData = Files::fetchMetaInformationForDirectory($fsPath);
+		$metaData = Files::fetchMetaInformationForDirectory($pagePathInFilesystem);
+		if (!isset($metaData['Layout'])) $metaData['Layout'] = 'Default';
+		if (!isset($metaData['Template'])) $metaData['Template'] = 'Default';
 
 		$this->session->putData('currentPath', $path);
-		$this->view->assign('currentPath', urldecode($path));
-		$this->view->assign('metaData', $metaData);
+
+		if ($externalPackage) {
+			$view = new \TYPO3\Fluid\View\TemplateView();
+
+			$controllerContext = clone $this->getControllerContext();
+			$controllerContext->getRequest()->setControllerPackageKey($packageKey);
+			$view->setControllerContext($controllerContext);
+
+			// $view->setLayoutPathAndFilename($packageResourcesDirectory.'Private/Templates/Page/'.$metaData['Layout'].'.html');
+			$view->setTemplatePathAndFilename($packageResourcesDirectory.'Private/Templates/'.$metaData['Template'].'.html');
+
+			$view->setLayoutRootPath($packageResourcesDirectory.'Private/Templates/Page/');
+			$view->setTemplateRootPath($packageResourcesDirectory.'Private/Templates/');
+			$view->setPartialRootPath($packageResourcesDirectory.'Private/Partials/');
+
+			$view->assign('currentPath', urldecode($path));
+			$view->assign('metaData', $metaData);
+
+			$view->canRender($this->getControllerContext());
+			return $view->render();
+		} else {
+			$this->view->assign('currentPath', urldecode($path));
+			$this->view->assign('metaData', $metaData);
+		}
 	}
 
 
