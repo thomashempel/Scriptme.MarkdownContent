3f25cedbb3c0feeb42db1ad1ffabd94bb69f4613
Patrick Kollodzik <patrick.kollodzik@googlemail.com>
2013-04-14 17:50:18 +0200

    [FEATURE] Make use of the Visible page information
    If the meta information Visible is set to False for a page,
    then will handle the as if it dosn't exists and a 404 status will be
    thrown.

--- Classes/Scriptme/MarkdownContent/Controller/StandardController.php
+++ Classes/Scriptme/MarkdownContent/Controller/StandardController.php
@@ -50,6 +50,7 @@ class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 	public function indexAction($path = '')
 	{
 		$externalPackage = FALSE;
+		$pageNotFound = FALSE;
 
 		if (isset($this->settings['SitePackage'])) {
 			$packageKey = $this->settings['SitePackage'];
@@ -66,32 +67,32 @@ class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 		$pagePathInFilesystem = $packageContentDirectory . $path;
 
 		if (!file_exists($pagePathInFilesystem) || !is_dir($pagePathInFilesystem)) {
-			//Test if a special errorHandling is configured and test if the configured path exists
-			if($this->settings['errorHandling']['404']) {
-				$path = str_replace(array('../', '.html'), '', $this->settings['errorHandling']['404']);
-				$pagePathInFilesystem = $packageContentDirectory . $path;
-
-				if (!file_exists($pagePathInFilesystem) || !is_dir($pagePathInFilesystem)) {
-					$this->throwStatus(404);
-				}
-				$this->response->setStatus(404);
-			}else {
-				$this->throwStatus(404);
-			}
+			$pageNotFound = TRUE;
+			$pagePathInFilesystem = $this->handle404($packageContentDirectory);
 		}
 
 		$metaData = Files::fetchMetaInformationForDirectory($pagePathInFilesystem);
+
+		if($pageNotFound == FALSE && $metaData['Visible'] == FALSE) {
+			$pagePathInFilesystem = $this->handle404($packageContentDirectory);
+			$metaData = Files::fetchMetaInformationForDirectory($pagePathInFilesystem);
+		}
+
 		if (!isset($metaData['Layout'])) $metaData['Layout'] = 'Default';
 		if (!isset($metaData['Template'])) $metaData['Template'] = 'Default';
 
 		//Create the Context, this will be use in the viewhelper
 		$this->context->setResourceDirectory($packageResourcesDirectory);
 		$this->context->setContentDirectory($packageContentDirectory);
-		$this->context->setCurrentPath($path);
+
+		if(!$this->context->getCurrentPath()) {
+			$this->context->setCurrentPath($path);
+		}
+
 		$this->context->setMetdaData($metaData);
 		$this->context->setSitePackageKey($packageKey);
 
-		$this->session->putData('currentPath', $path);
+		$this->session->putData('currentPath', $this->context->getCurrentPath());
 
 		$this->view->setTemplatePathAndFilename($packageResourcesDirectory.'Private/Templates/'.$metaData['Template'].'.html');
 
@@ -99,11 +100,30 @@ class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 		$this->view->setTemplateRootPath($packageResourcesDirectory.'Private/Templates/');
 		$this->view->setPartialRootPath($packageResourcesDirectory.'Private/Partials/');
 
-		$this->view->assign('currentPath', urldecode($path));
+		$this->view->assign('currentPath', urldecode($this->context->getCurrentPath()));
 		$this->view->assign('metaData', $metaData);
 	}
 
+	/**
+	 * Test if a special errorHandling is configured and test if the configured path exists
+	 * @param string $packageContentDirectory
+	 */
+	protected function handle404($packageContentDirectory) {
+		//Test if a special errorHandling is configured and test if the configured path exists
+		if ($this->settings['errorHandling']['404']) {
+			$path = str_replace(array('../', '.html'), '', $this->settings['errorHandling']['404']);
+			$pagePathInFilesystem = $packageContentDirectory . $path;
 
+			if (!file_exists($pagePathInFilesystem) || !is_dir($pagePathInFilesystem)) {
+				$this->throwStatus(404);
+			}
+			$this->response->setStatus(404);
+			$this->context->setCurrentPath($path);
+			return $pagePathInFilesystem;
+		} else {
+			$this->throwStatus(404);
+		}
+	}
 
 }
 
